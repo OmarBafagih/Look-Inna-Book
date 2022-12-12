9e8df22abfe2d861f42711b8b116a947e3f7cc97
Omar Bafagih <bafagihomar260@gmail.com>
2022-12-11 23:10:12 -0500

    Added front-end for customers and created db modifications

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const client = new Client({
 })
 
 
-
+client.connect();
 
 
 const path = require('path');
+const { nextTick } = require('process');
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, "public")));
@@ -26,23 +27,33 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', function (req, res){res.render('home')});
 app.get('/storeOwner', function (req, res){res.render('storeOwner')});
 app.get('/register', function (req, res){res.render('register')});
-app.get('/customer', function (req, res){
+app.get('/customer', function (req, response){
     //load data from db into pug file to show books
-    res.render('customer');
+    try{
+        client.query(`Select * FROM book`, (err, res) =>{
+            if(!err){
+                console.log(res.rows);
+                response.render('customer', {books: res.rows});
+            }
+            else{
+                console.log(err.message);
+            }
+        }) 
+    } catch(err){
+        next(err);
+    }
 
 });
 
 //POST REQUESTS
 app.post("/customer", function(req,res){
     console.log(req.body);
-    
-
-
     res.status(200).send();
 });
 
 app.post("/removeBooks", function(req,res){
     console.log(req.body);
+
     res.status(200).send();
 });
 
@@ -56,9 +67,11 @@ app.post("/addBooks", function(req,res){
     res.status(200).send();
 });
 
+
+
 const insertBook = async (newBook) => {
     try {
-        await client.connect();
+        // await client.connect();
         await client.query(
             `INSERT INTO "book"   
              VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [newBook.ISBN, newBook.bookName, newBook.authorName, newBook.genre, newBook.publisherName, newBook.price, newBook.pages, newBook.transferPercentage, newBook.quantity]);
@@ -66,11 +79,21 @@ const insertBook = async (newBook) => {
     } catch (error) {
         console.error(error.stack);
         return false;
-    } finally {
-        await client.end();
     }
 };
 
+const removeBook = async (newBook) => {
+    try {
+        // await client.connect();
+        await client.query(
+            `UPDATE INTO "book"   
+             VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [newBook.ISBN, newBook.bookName, newBook.authorName, newBook.genre, newBook.publisherName, newBook.price, newBook.pages, newBook.transferPercentage, newBook.quantity]);
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
 
 
 
