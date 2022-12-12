ac33ee372f642212ae1a757ebe3420cd804ea75a
Braeden-Cheung <120347252+Braeden-Cheung@users.noreply.github.com>
2022-12-12 00:33:11 -0500

    Update Functions.sql

--- SQL/Functions.sql
+++ SQL/Functions.sql
@@ -9,16 +9,34 @@ BEGIN
 END;
 $$
 
+/* creates an order */
 CREATE FUNCTION create_order()
 	RETURNS TRIGGER
 	LANGUAGE PLPGSQL
 AS $$
 BEGIN
-	INSERT INTO SUBMITORDER(orderNum,userName,cartID,reportID)VALUES(DEFAULT,<userName>,curr(pg_get_serial_sequence('cart','cartid')),DEFAULT);
-	
+/* inserting reportID, S(sale), and total amount of order into REPORT TABLE */
 	INSERT INTO REPORT(reportID,expenseOrSale,amount)
-	SELECT curr(pg_get_serial_sequence('SUBMITORDER','reportID'),'S',sum(b.ISBN*b.price)
+	SELECT curr(pg_get_serial_sequence('SUBMITORDER','reportID')),'S',sum(b.ISBN*b.price)
 	FROM BOOK b, BOOKCONTAINS bk, CART c
 	WHERE C.cartID = bk.cartID and bk.ISBN = b.ISBN;
 
-	DELETE FROM 
+/* giving customer new cart */
+	INSERT INTO CART(userName,cartID)VALUES(<userName>,DEFAULT);
+	
+	RETURN NULL;
+END;
+$$
+
+/* making report from expense */
+CREATE FUNCTION create_expense_report()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+AS$$
+BEGIN
+	
+	INSERT INTO REPORT(reportID,expenseOrSale,amount)VALUE(curr(pg_get_serial_sequence('EXPENSES','reportid')),'E',<amount>)
+
+	RETURN NULL;
+END;
+$$
